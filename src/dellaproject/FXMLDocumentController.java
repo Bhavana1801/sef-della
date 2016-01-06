26891759bf0c6bc01850dc5e374d86bd0d506034
Bhavana1801 <bhavana.manchana@gmail.com>
2016-01-06 16:37:37 +0530

    clear and actionitemlist

--- src/dellaproject/FXMLDocumentController.java
+++ src/dellaproject/FXMLDocumentController.java
@@ -8,11 +8,15 @@ package dellaproject;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -44,6 +48,9 @@ public class FXMLDocumentController implements Initializable {
     @FXML private Label console_Status;
     @FXML private Label console_memberDetails;
     @FXML private Label console_teamDetails;
+    @FXML private TextField console_name;
+    @FXML private TextArea console_description;
+    @FXML private TextArea console_resolution;
     //action items screen
     @FXML private ComboBox<String> action_actionItems;
     @FXML private final ComboBox<String> action_inclusionFactor;
@@ -57,6 +64,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML private ComboBox<String> action_Team;
     @FXML private TextField action_dueDate;
 //    private final ListView console_actionItemList;
+   
     public FXMLDocumentController() {
         this.action_inclusionFactor = new ComboBox<>();
         this.action_sortingDirection = new ComboBox<>();
@@ -115,6 +123,12 @@ public class FXMLDocumentController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        try {
+            consoleDisplay();
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     public void saveForm(ActionEvent event) throws SQLException {
         System.out.println("create a new action form");
@@ -127,7 +141,45 @@ public class FXMLDocumentController implements Initializable {
         sql = "INSERT INTO sample VALUES('"+name+"','"+Description+"','"+Resolution+"')";
         st.executeUpdate(sql);
     }
-    public void clearForm(ActionEvent event){
+    public void clearForm(ActionEvent event) throws SQLException{
         System.out.println("clear this form");
+        action_Name.setText("");
+        action_Description.setText("");
+        action_Resolution.setText("");
+//        consoleDisplay();
     }
+    public void consoleDisplay() throws SQLException {
+        System.out.println("console display");
+        Connection con2 = connect();
+        
+        String str = "SELECT name FROM sample";
+        ResultSet rs = con2.createStatement().executeQuery(str);
+        ObservableList<String> itemList = FXCollections.observableArrayList();
+        while(rs.next()){
+            String name = rs.getString("name");
+            System.out.println(name);
+            itemList.add(name);
+        }
+        console_actionItemList.setItems(itemList); 
+    }
+    public void consoleSelectItem() throws SQLException{
+        System.out.println("an item is selected from console");
+        String item = console_actionItemList.getSelectionModel().getSelectedItem();
+        System.out.println(item);
+        Connection con3 = connect();
+        String str = "SELECT * FROM sample where name='"+item+"'";
+        ResultSet rs = con3.createStatement().executeQuery(str);
+        while(rs.next()){
+            String name = rs.getString("name");
+            String desc = rs.getString("description");
+            String res = rs.getString("resolution");
+            
+            System.out.println(res+"jdash");
+            console_name.setText(name);
+            console_description.setText(desc);
+            console_resolution.setText(res);
+            
+        }
+    }
+    
 }
