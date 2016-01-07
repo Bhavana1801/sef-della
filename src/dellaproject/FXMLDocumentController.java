1587e20785a9fc24da90540950d9d4d57ee239de
Bhavana1801 <bhavana.manchana@gmail.com>
2016-01-07 12:07:14 +0530

    actionItems and console updations are done

--- src/dellaproject/FXMLDocumentController.java
+++ src/dellaproject/FXMLDocumentController.java
@@ -20,6 +20,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -36,9 +39,6 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     //console screen
     @FXML ListView<String> console_actionItemList;
-//    @FXML private TextField console_Name;
-//    @FXML private final TextField console_Description;
-//    @FXML private TextField console_Resolution;
     @FXML private ComboBox<String> console_inclusionFactor;
     @FXML private ComboBox<String> console_sortingDirection;
     @FXML private ComboBox<String> console_firstSortingFactor;
@@ -63,8 +63,28 @@ public class FXMLDocumentController implements Initializable {
     @FXML private ComboBox<String> action_Member;
     @FXML private ComboBox<String> action_Team;
     @FXML private TextField action_dueDate;
-//    private final ListView console_actionItemList;
-   
+    @FXML private Button action_Update;
+
+    //members screen
+    
+    @FXML private TextField members_name;
+    @FXML private ListView<String> members_indiv;
+    @FXML private ListView<String> members_avail;
+    @FXML private ListView<String> members_current;
+    @FXML private Button members_addToList;
+    @FXML private Button members_removeFromList;
+    @FXML private Button members_addAffliation;
+    @FXML private Button members_removeAffliation;
+     //teams screen variables
+    
+    @FXML private TextField teams_name;
+    @FXML private ListView<String> teams_known;
+    @FXML private ListView<String> teams_avail;
+    @FXML private ListView<String> teams_current;
+    @FXML private Button teams_addToList;
+    @FXML private Button teams_removeFromList;
+    @FXML private Button teams_addAssociation;
+    @FXML private Button teams_removeAssociation;
     public FXMLDocumentController() {
         this.action_inclusionFactor = new ComboBox<>();
         this.action_sortingDirection = new ComboBox<>();
@@ -130,6 +150,7 @@ public class FXMLDocumentController implements Initializable {
         }
         
     }
+    //the form is saved-action item form
     public void saveForm(ActionEvent event) throws SQLException {
         System.out.println("create a new action form");
        Connection con1 =  connect();
@@ -137,17 +158,50 @@ public class FXMLDocumentController implements Initializable {
        String name = action_Name.getText();
        String Description = action_Description.getText();
        String Resolution = action_Resolution.getText();
-       String sql;
+       String sql = "SELECT name FROM sample";
+        ResultSet rs = con1.createStatement().executeQuery(sql);
+        ObservableList<String> itemList = FXCollections.observableArrayList();
+        while(rs.next()){
+            String str = rs.getString("name");
+//            System.out.println(name);
+            itemList.add(str);
+        }
+        System.out.println(itemList);
+        System.out.println(name);
+        //if the action item name already exists alert message is shown.
+        if(itemList.contains(name)) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Name already exists");
+            alert.setContentText("please select another name for the action item");
+
+            alert.showAndWait();
+            return;
+        }
+        // if the action item name is empty, warning is shown
+        else if(name.equals("")) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Name cannot be empty");
+            alert.setContentText("please select ar name for your action item");
+
+            alert.showAndWait();
+            return;
+        }
+        
         sql = "INSERT INTO sample VALUES('"+name+"','"+Description+"','"+Resolution+"')";
         st.executeUpdate(sql);
+        st.close();
+//        st.executeUpdate(sql);
     }
+    //the form is cleared when we click clear form button
     public void clearForm(ActionEvent event) throws SQLException{
         System.out.println("clear this form");
         action_Name.setText("");
         action_Description.setText("");
         action_Resolution.setText("");
-//        consoleDisplay();
     }
+    //displays list of action items list on console screen
     public void consoleDisplay() throws SQLException {
         System.out.println("console display");
         Connection con2 = connect();
@@ -162,6 +216,7 @@ public class FXMLDocumentController implements Initializable {
         }
         console_actionItemList.setItems(itemList); 
     }
+    //details of a particular item selected is shown on console screen
     public void consoleSelectItem() throws SQLException{
         System.out.println("an item is selected from console");
         String item = console_actionItemList.getSelectionModel().getSelectedItem();
@@ -180,6 +235,60 @@ public class FXMLDocumentController implements Initializable {
             console_resolution.setText(res);
             
         }
+    }
+    //the action item form is updated.
+    public void updateActionItem() throws SQLException {
+        System.out.println("update action form");
+       Connection con1 =  connect();
+       Statement st = con1.createStatement();
+       String name = action_Name.getText();
+       String Description = action_Description.getText();
+       String Resolution = action_Resolution.getText();
+       String sql = "UPDATE sample SET description='"+Description+"',resolution='"+Resolution+"'WHERE name='"+name+"'";
+       System.out.println(name+Description+Resolution);
+        ResultSet rs = con1.createStatement().executeQuery(sql);
+        
+    }
+    public void displayActionItemScreen() throws SQLException {
+        System.out.println("action item screen is clicked");
+        String item = console_actionItemList.getSelectionModel().getSelectedItem();
+        System.out.println(item);
+        Connection con3 = connect();
+        String str = "SELECT * FROM sample where name='"+item+"'";
+        ResultSet rs = con3.createStatement().executeQuery(str);
+        while(rs.next()){
+            String name = rs.getString("name");
+            String desc = rs.getString("description");
+            String res = rs.getString("resolution");
+            
+//            System.out.println(res+"jdash");
+            action_Name.setText(name);
+            action_Description.setText(desc);
+            action_Resolution.setText(res);
+            
+        }
+    }
+    // addToList button is disabled when user clicks on right side box
+    public void disableAddToList() {
+        members_addToList.setDisable(true);
+        teams_addToList.setDisable(true);
+    }
+    // removeFromList button is disabled when user clicks on left side box
+    public void disableRemoveFromList() {
+        members_removeFromList.setDisable(true);
+    }
+    // addAffliation button is disabled when user clicks on right side box
+    public void disableAddAffliation() {
+        members_addAffliation.setDisable(true);
+    }
+    // removeAffliation button is disabled when user clicks on left side box
+    public void disableRemoveAffliation() {
+        members_removeAffliation.setDisable(true);
+    }
+    
+    //application is closed
+    public void closeApplication(ActionEvent event) {
+        
     }
     
 }
